d54d70d16689e4a9913c6084afddb1f8bad462a4
renneee <renpyang@gmail.com>
2017-09-30 08:42:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,8 +9,12 @@ function nestedTarget () {
 function increaseRankBy (n) {
   const lis = document.querySelectorAll(ranked-list)
 
-  for (let n = 0, n < lis.length; i++) {
-    lis[n].innerHTML = (n-1).tostring()
+  for (let i = 0; i < lis.length; i++) {
+    let children = lis[i].children
+
+    for (let j=0; j < children.length; j++) {
+      children[j].innerHTML = parseInt(children[j].innerHTML) + n;
+    }
   }
 }
 
